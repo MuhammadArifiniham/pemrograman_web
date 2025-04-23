c21f995389976afe43b2af2588447571958d2900
MuhammadArifiniham <ilhamarifin2119@gmail.com>
2025-04-23 14:36:44 +0700

    Add files via upload

---
+++ pemrograman_web II/latihan 1 & 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+	int main ()
+{
+	int a = 5; 
+	int b = 7;
+	int c = 2;
+	int x = 8;
+	int y = 3;
+	
+	double result1= (-2+a+b)/c;
+	double result2= sqrt(a / b) + pow(c, 2);
+    cout<<"Hasil 1= "<<result1<<endl;
+    cout<<"Hasil 2= "<<result2<<endl;
+}
